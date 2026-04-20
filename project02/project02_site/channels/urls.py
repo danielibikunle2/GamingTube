80271b634e2d5c08d80ed0391f449f62cc6483aa
daniel ibikunle <daniel.ibikunle2@mail.dcu.ie>
2026-04-20 19:57:26 +0100

    made it so the user can make channels

--- project02/project02_site/channels/urls.py
+++ project02/project02_site/channels/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.channel_list, name='channel_list'),
     path('<int:pk>/', views.channel_detail, name='channel_detail'),
+    path('add/', views.channel_add, name='channel_add'),
 ]
--- project02/project02_site/channels/views.py
+++ project02/project02_site/channels/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Channel
+from .forms import ChannelForm
+
 
 def channel_list(request):
     channels = Channel.objects.all()
@@ -10,3 +12,16 @@ def channel_detail(request, pk):
     channel = get_object_or_404(Channel, pk=pk)
     context = {'channel': channel}
     return render(request, 'channels/channel_detail.html', context)
+
+def channel_add(request):
+    if request.method == 'POST':
+        form = ChannelForm(request.POST, request.FILES)
+        if form.is_valid():
+            channel = form.save(commit=False)
+            channel.owner = request.user
+            channel.save()
+            return redirect('channel_list')
+    else:
+        form = ChannelForm()
+    context = {'form': form}
+    return render(request, 'channels/channel_add.html', context)
